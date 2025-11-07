25519a4a9805a3ff0f8f394cbd4dcbfb52ab1971
Alberth Nery <iophyhd@gmail.com>
2025-11-07 12:16:41 -0300

    refactor: alterado componente 'Seguranca' do 'App.jsx' -> 'Home.jsx'

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Home from "./components/Home/Home.jsx";
-import Seguranca from "./components/Home/Seguranca/Seguranca.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <Home />
-      <Seguranca />
       <Footer />
     </>
   );
