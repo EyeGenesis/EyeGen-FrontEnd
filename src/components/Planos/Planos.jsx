c00fbce0a839ee03e3053d6be6b81d02a1b11f74
Alberth Nery <107318708+asmnery@users.noreply.github.com>
2025-11-12 18:41:29 -0300

    feat: add componente 'HeroPlanos' na planos

---
+++ src/components/Planos/Planos.jsx
@@ -0,0 +1,11 @@
+import style from "./Planos.module.css";
+import HeroPlanos from "./HeroPlanos/HeroPlanos";
+
+export default function Planos() {
+  return (
+    <main>
+        <HeroPlanos />
+
+    </main>
+  );
+}
