a7b7bcd2a8fbafa5323072abb146a26464bcdde3
Alberth Nery <107318708+asmnery@users.noreply.github.com>
2025-11-12 16:11:06 -0300

    feat: preparado arquivos 'main.jsx', 'App.jsx' e componente 'Header' com react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Home from "./components/Home/Home.jsx";
+import Planos from "./components/Planos/Planos.jsx";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <Header />
-      <Home />
-      <Footer />
+    <Header />
+    
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/planos" element={<Planos />} />
+    </Routes>
+
+    <Footer />
     </>
   );
 }
