4b08813437cc8e52a5209c1c1c0b23d6eaa36d4c
Alberth Nery <107318708+asmnery@users.noreply.github.com>
2025-11-14 23:04:06 -0300

    feat: add componente 'CentralAjuda' na suporte

--- src/components/Suporte/Suporte.jsx
+++ src/components/Suporte/Suporte.jsx
@@ -1,12 +1,14 @@
 import style from "./Suporte.module.css";
 import HeroSuporte from "./HeroSuporte/HeroSuporte";
 import GuiasUso from "./GuiasUso/GuiasUso";
+import CentralAjuda from "./CentralAjuda/CentralAjuda";
 
 export default function Suporte() {
   return (
     <main>
         <HeroSuporte />
         <GuiasUso />
+        <CentralAjuda />
     </main>
   );
 }
