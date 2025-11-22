cab65f2df58c31759c0d460a6657992cd022fadf
MatheusDias10 <matheussilvadias2007@gmail.com>
2025-11-21 23:06:41 -0300

    fix: Removendo a página de Login da branch Cadastro para não haver conflito na hora do merge

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Home from "./components/Home/Home.jsx";
-import Login from "./components/Login/Login.jsx";
 import Cadastro from "./components/Cadastro/Cadastro.jsx";
 
 function App() {
   return (
     <>
-      <Login />
+      <Cadastro />
     </>
   );
 }
